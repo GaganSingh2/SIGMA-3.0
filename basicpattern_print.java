db95144f3439220f3858952c811cdf58fc9c6ed8
GaganSingh2 <gks022004@gmail.com>
2024-07-11 13:09:07 +0530

    Print_Basic-Pattern

---
+++ basicpattern_print.java
@@ -0,0 +1,8 @@
+public class basicpattern_print {
+    public static void main(String[] args) {
+        System.out.println("XXXX");
+        System.out.println("XXX");
+        System.out.println("XX");
+        System.out.println("X");
+    }
+}
