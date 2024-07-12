28d1fac46dae9c52d40b160bed6d196bf2b3e333
GaganSingh2 <gks022004@gmail.com>
2024-07-12 23:59:13 +0530

    "QusOfVariavleDataTypes"

---
+++ qus_variable_datatype.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+
+public class qus_variable_datatype {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //Q1) In a program input 3 number: A,B and C. you have to output the average of these 3 numbers
+        // System.out.println("Enter your number of A: ");
+        // int A = sc.nextInt();
+
+        // System.out.println("Enter your number of B: ");
+        // int B = sc.nextInt();
+
+        // System.out.println("Enter your number of C: ");
+        // int C = sc.nextInt();
+
+        // float avg = (A+B+C)/3;
+        // System.out.println(avg);
+
+        //Q2) In a program input the side of square. you have to output the area of the square..
+
+        // System.out.println("Enter the value of side: ");
+        // int side = sc.nextInt();
+
+        // int square = side * side;
+        // System.out.println("Square of given side is "+square);
+
+        //Q3) Enter cost of 3 items from the user(using float type) a pencil,a pen,an eraser. you have to output the total cost of the item back to the user as their bill(and also include 18% gst tax)
+
+        // System.out.print("ENter the cost of Pencil: ");
+        // float pencil = sc.nextFloat();
+
+        // System.out.print("ENter the cost of Pen: ");
+        // float pen = sc.nextFloat();
+
+        // System.out.print("ENter the cost of Eraser: ");
+        // float eraser = sc.nextFloat();
+
+        // float total_cost = pencil + pen + eraser;
+        // System.out.println("The Total cost of the items is "+total_cost);
+
+        // float new_total = total_cost + (0.18f * total_cost);
+        // System.out.println("The total cost of the item with 18% tax is "+new_total);
+
+        //Q4)WAP to calculate percentage of a given student in CBSE board exam. His marks from 5 subjects must be taken as input from the keyboard
+
+        // System.out.print("Enter the marks of English: ");
+        // int english = sc.nextInt();
+
+        // System.out.print("Enter the marks of Hindi: ");
+        // int hindi = sc.nextInt();
+
+        // System.out.print("Enter the marks of Math: ");
+        // int math = sc.nextInt();
+
+        // System.out.print("Enter the marks of Physics: ");
+        // int physics = sc.nextInt();
+
+        // System.out.print("Enter the marks of Chemistry: ");
+        // int chemistry = sc.nextInt();
+
+        // int obtain_marks = (english + hindi + math + physics + chemistry);
+        
+        // System.out.println("THe total Obtain marks is "+obtain_marks);
+
+        // float total_marks = 500f;
+        // float percentage = (obtain_marks / total_marks)* 100;
+
+        // System.out.println(percentage);
+
+
+        // Q5) Will the following statement give any error in java
+         int $ = 24;
+         System.out.println($);
+        // No, the statement will not give any error. because it is a fullfill the function of identifiers in variable.
+    }
+}
