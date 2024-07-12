7ee7e18bd91efc1a4192905dcf4ec4093fb2d06c
GaganSingh2 <gks022004@gmail.com>
2024-07-12 23:57:46 +0530

    "DataTypes"

---
+++ data_types.java
@@ -0,0 +1,30 @@
+public class data_types {
+    public static void main(String[] args) {
+        System.out.println("-----DATA TYPES IN JAVA------");
+        // byte:- size 1-byte, range -128 to 127
+        System.out.println("Byte:- It can be contain -128 to 127");
+
+        // short:- size 2-byte
+        System.out.println("Short:- It can be contain -32,768 to 32,767");
+        
+        // char: size 2-byte, range a to z, @ ,% etc. 
+        System.out.println("Char:- It can be contain single character");
+
+        // boolean:- size 1-byte, range true or false
+        System.out.println("Boolean:- it can be contain true or false");
+
+        // int:- size 4-byte, range -2B to 2B
+        System.out.println("Int:- it can be contain without decimal value");
+
+        // long:- size 8-byte, range more than of int
+        System.out.println("long:- it can be contain the more then value of long");
+
+        // float:- size 4-byte, range with decimal before 3 number
+        System.out.println("Float:- it can be contain the value with decimal before 8 numbers");
+
+        // double:- size 8-byte, range with decimal before 6 to 8 numbers
+        System.out.println("Double:- it can be contain the value with decimal before 15 numbers");
+
+
+    }
+}
