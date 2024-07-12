83127c921e8f928904dfeabeb9aa5e0abfb289d3
GaganSingh2 <gks022004@gmail.com>
2024-07-13 02:20:35 +0530

    "QusOfConditionalStatement"

---
+++ qus_conditional_statement.java
@@ -0,0 +1,110 @@
+import java.util.Random;
+import java.util.Scanner;
+public class qus_conditional_statement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //Q1) WAP a java program to get a number from the user and print whether it is positive and negative
+
+        // System.out.println("Enter your numer: ");
+        // int num = sc.nextInt();
+
+        // if (num==0) {
+        //     System.out.println("the number is neither positive or negative");
+        // }
+        // else if(num<0){
+        //     System.out.println(num+" is negative.");
+        // }
+        // else{
+        //     System.out.println(num+" is positive.");
+        // }
+
+        //Q2) Finish the following code so that it prints you have a fever if your temperature is above 100 and otherwise prints you don't have a fever.
+
+        // System.out.print("Enter your temperature: ");
+        // float temp = sc.nextFloat();
+        
+        // if (temp>100.0) {
+        //     System.out.println("YOu have a fever");
+        // }
+        // else{
+        //     System.out.println("you have not fever");
+        // }
+
+        //Q3)WAP in java to input week name and print the day of week name using switch case
+
+        // System.out.print("Enter the day name: ");
+        // String day = sc.nextLine();
+
+        // switch (day) {
+        //     case "sunday":
+        //         System.out.println("Today is Sunday");
+        //         break;
+        //     case "monday":
+        //         System.out.println("Today is Monday");
+        //         break;
+        //     case "tuesday":
+        //         System.out.println("Today is Tuesday");
+        //         break;
+        //     case "wednesday":
+        //         System.out.println("Today is Wednesday");
+        //         break;
+        //     case "thursday":
+        //         System.out.println("Today is Thursday");
+        //         break;
+        //     case "friday":
+        //         System.out.println("Today is Friday");
+        //         break;
+        //     case "saturday":
+        //         System.out.println("Today is Saturday");
+        //         break;
+        //     default:
+        //         System.out.println("Wrong input");
+        //         break;
+        // }
+
+
+        //Q5) WAP in java program that takes a year from the user and print whether that year is a leap year or not.
+
+        // System.out.println("Enter the year: ");
+        // int year = sc.nextInt();
+
+        // if ((year % 4==0) && (year % 100==0) && (year %400==0)) {
+        //     System.out.println(year+" is a leap year");
+        // }
+        // else if ((year % 4==0) && (year % 100!=0) || (year % 400==0)) {
+        //     System.out.println(year+" is a leap year");
+        // }
+        // else{
+        //     System.out.println(year+" is not a leap year");
+        // }
+
+        //Q6)Write a program that prints numbers from 1 to 100. For multiples of 3, print "Fizz" instead of the number, and for multiples of 5, print "Buzz". For numbers divisible by both 3 and 5, print "FizzBuzz"
+
+        // System.out.println("ENter the numbe: ");
+        // int num = sc.nextInt();
+
+        // if((num%3==0)&&(num%5==0)){
+        //     System.out.println("FizzBuzz");
+        // }
+        // else if (num%5==0) {
+        //     System.out.println("Buzz");
+        // }
+        // else if (num%3==0) {
+        //     System.out.println("Fizz");
+        // }
+        // else{
+        //     System.out.println(num+" is not divisible by both");
+        // }
+
+
+        //Q6)
+       
+
+
+        
+    
+
+
+    }
+}
