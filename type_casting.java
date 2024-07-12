968e40123154c99ea2ec8d59c1fcd327562d70f2
GaganSingh2 <gks022004@gmail.com>
2024-07-12 23:59:34 +0530

    "TYpeCasting"

---
+++ type_casting.java
@@ -0,0 +1,9 @@
+public class type_casting {
+    public static void main(String[] args) {
+        // ------TYPE CASTING------
+        // it is also called Explicit casting
+        float marks = 99.99999f;
+        int marks2 = (int)marks;
+        System.out.println("Your marks in type of int: "+marks2);
+    }
+}
