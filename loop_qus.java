da232869dff680b3774b867c2956f3025a9e7364
GaganSingh2 <gks022004@gmail.com>
2024-07-14 01:56:59 +0530

    "LoopQuestion"

---
+++ loop_qus.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class loop_qus {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //Q1) How many times "Hello" is printed
+        // for(int i=0; i<5; i++){
+        //     System.out.println("Hello");
+        //     i += 2;
+        // }
+
+        //Q2)WAP to program that reads a set of integer, and then prints the sum of the even and odd integers
+
+        // int num;
+        // int evenSum = 0;
+        // int oddSum = 0;
+        // int choice;
+        // do{
+        //     System.out.println("ENter the number: ");
+        //     num = sc.nextInt();
+            
+        //     if (num%2==0) {
+        //         evenSum += num;
+        //     }
+        //     else{
+        //         oddSum += num;
+        //     }
+        //     System.out.println("Can you continue press 1 for yess or 2 for No");
+        //     choice = sc.nextInt();
+        // }while(choice==1);
+        // System.out.println("Your total sum of even number is "+evenSum);
+        // System.out.println("Your total sum of odd number is "+oddSum);
+
+
+        //Q3) Write a program to find the factorial of any number entered by the user.
+        
+        // int num;
+        // int fact = 1;
+        // System.out.println("ENter any positive integer: ");
+        // num = sc.nextInt();
+        // for(int i=1; i<=num; i++){
+        //     fact *= i;
+        // }
+        // System.out.println("Factorial: "+fact);
+
+
+        //Q4)WAP to print the multiplication table of a number N, entered by the user.
+
+        // System.out.println("Enter your table number: ");
+        // int num = sc.nextInt();
+
+        // for(int i=1; i<=10; i++){
+        //     System.out.println(num+" X "+i+" = "+num*i);
+        // }
+
+        
+    }
+}
