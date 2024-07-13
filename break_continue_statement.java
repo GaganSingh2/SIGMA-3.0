1f696a2a398cb29301439ae34fab1f778c1f9c4a
GaganSingh2 <gks022004@gmail.com>
2024-07-14 01:56:36 +0530

    "Break_and_ContinueStatement"

---
+++ break_continue_statement.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class break_continue_statement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // ---------BREAK STATEMENT
+
+        // System.out.println("Enter your num: ");
+        // int num = sc.nextInt();
+        // System.out.println("Enter breaking num: ");
+        // int bre = sc.nextInt();
+
+        // for(int counter=1; counter<=num; counter++){
+        // if (counter==bre) {
+        // break;
+        // }
+        // System.out.println(counter);
+        // }
+        // System.out.println("I am out of loop");
+
+        // Q)WAP to keep entering numbers till user enters a multiple of 10
+        // int num;
+        // do {
+        // System.out.println("Enter your number: ");
+        // num = sc.nextInt();
+
+        // if (num%10==0) {
+        // break;
+        // }
+        // System.out.println(num+" is not multiple of 10");
+
+        // }while(true);
+        // System.out.println(num+" is multiple of 10");
+
+
+        //--------CONTINUE STATEMENT------
+
+        // System.out.println("Emter your times:");
+        // int times = sc.nextInt();
+        // System.out.println("Enter your skiping number: ");
+        // int skip = sc.nextInt();
+        // for(int counter=1; counter<=times; counter++){
+        //     if (counter==skip) {
+        //         continue;
+        //     }
+        //     System.out.println(counter);
+        // }
+
+
+        //Q)WAP to display all numbers entered by user except multiple of 10.
+
+        // do{
+        //     System.out.println("ENter your number: ");
+        //     int num = sc.nextInt();
+
+        //     if (num%10==0) {
+        //         continue;
+                
+        //     }
+        //     System.out.println(num+" is not multiple of 10");
+        // }while(true);
+
+
+        
+
+    }
+}
