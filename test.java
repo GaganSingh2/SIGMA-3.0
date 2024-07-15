822d6ded56af94f28648c8871513176bc8c024e1
GaganSingh2 <gks022004@gmail.com>
2024-07-15 21:41:43 +0530

    "Test"

---
+++ test.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+import javax.swing.text.Style;
+
+public class test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //Q)Given number is prime or not.
+        System.out.println("Enter the number: ");
+        int num = sc.nextInt();
+        if (num==2) {
+            System.out.println(num+" is a prime number");
+        }
+        else{
+            boolean isPrime = true;
+            for(int i=2; i<=num-1;i++){
+                if (num%i==0) {
+                    isPrime = false;
+                }
+                
+            }
+            if (isPrime==true) {
+                System.out.println(num+" is a prime number");
+            }
+            else{
+                System.out.println(num+" is not a prime number");
+            }
+        }
+    }
+}
