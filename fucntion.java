6593d0e4e1ae6313377d96b2ddcf9daa06b08bb2
GaganSingh2 <gks022004@gmail.com>
2024-07-16 00:04:17 +0530

    "SyntaxOfFunction"

---
+++ fucntion.java
@@ -0,0 +1,24 @@
+public class fucntion {
+
+    /*Syntax Of Fuction:-
+     *           returnType name(){
+     *                   body
+     *                   return statement; 
+     *           }
+     */
+
+
+    //  public static void printHello(){
+    //     System.out.println("Hello World");
+    //     return;  //humne yaha simple return likha hai kyuki void empty value return karta hai
+    //  }
+    public static int printHello(){
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        return 3;  //humne yaha retrun ke sath 3 likha hai kyuki humne apne function me returntype int liya hai jo kuch na kuch value return karayega
+    }
+    public static void main(String[] args) {
+        printHello();  //function call
+    }
+}
