c6ac5c227ddb0ffc2b5e2ba8601c3f56d8257ea1
GaganSingh2 <gks022004@gmail.com>
2024-07-24 15:02:51 +0530

    "Reverse the Array"

---
+++ reverseOfArray.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class reverseOfArray {
+
+    public static void reverse_Array(int arr[]){
+        int start = 0;
+        int end = arr.length - 1;
+        while (start<end) {
+            int temp = arr[end];
+            arr[end] = arr[start];
+            arr[start] = temp;
+
+            start++;
+            end--;
+        }
+        
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Find the Reverse of Given Array
+        System.out.println("Enter the length of Array: ");
+        int len = sc.nextInt();
+
+        int arr[] = new int[len];
+
+        System.out.println("ENter the value in Array: ");
+        for(int i=0; i<len; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        reverse_Array(arr);
+        System.out.println("After the Reverse of Given Array: ");
+        for(int i=0; i<len; i++){
+            System.out.println(arr[i]+" ");
+        }
+
+    }
+}
