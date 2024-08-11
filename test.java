775fb82bfedb83cd68da2c85fa44aaab716e8578
GaganSingh2 <gks022004@gmail.com>
2024-08-11 23:40:20 +0530

    "some question"

--- test.java
+++ test.java
@@ -3,30 +3,32 @@ import java.util.Scanner;
 import javax.swing.text.Style;
 
 public class test {
+    public static void insertionSort(int arr[]){
+        for(int i=1; i<arr.length; i++){
+            int curr = arr[i];
+            int prev = i-1;
+            //Finding out the correct position to insert
+            while (prev >= 0 && arr[prev] > curr) {
+                arr[prev+1] = arr[prev];
+                prev--;
+            }
+            //for insertion
+            arr[prev+1] = curr;
+        }
+    }
+    public static void printArr(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+        
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        //Q)Given number is prime or not.
-        System.out.println("Enter the number: ");
-        int num = sc.nextInt();
-        if (num==2) {
-            System.out.println(num+" is a prime number");
-        }
-        else{
-            boolean isPrime = true;
-            for(int i=2; i<=num-1;i++){
-                if (num%i==0) {
-                    isPrime = false;
-                }
-                
-            }
-            if (isPrime==true) {
-                System.out.println(num+" is a prime number");
-            }
-            else{
-                System.out.println(num+" is not a prime number");
-            }
-        }
+        int arr[] = {5,4,1,3,2};
+        insertionSort(arr);
+        printArr(arr);
+       
 
         //Q2) CHeck the given integer is palindrome or not
     }
