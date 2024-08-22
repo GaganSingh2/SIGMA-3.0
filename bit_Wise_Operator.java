f2791b568d4b7ce71af0c1f966538fc5eb11c615
GaganSingh2 <gks022004@gmail.com>
2024-08-22 23:50:27 +0530

    "Binary OR"

--- bit_Wise_Operator.java
+++ bit_Wise_Operator.java
@@ -9,12 +9,23 @@ public class bit_Wise_Operator {
         //---------BINARY AND(&)-----------
         /* Each bit in the result is set to 1 only if the corresponding bits in both input numbers are 1. Otherwise, the bit in the result is set to 0. in this program 1stly we convert the both number in binary number and then perform the AND operation */
 
-        System.out.println("Enter the first number: ");
+        // System.out.println("Enter the first number: ");
+        // int num1 = sc.nextInt();
+        // System.out.println("ENter the second number: ");
+        // int num2 = sc.nextInt();
+
+        // int result = (num1 & num2);
+        // System.out.println("Result is: "+result);
+
+ 
+        //------------BINARY OR(|)----------
+        /*Each bit in the result is set to 1 if at least one of the corresponding bits in the input numbers is 1. */
+        System.out.println("ENter the first number: ");
         int num1 = sc.nextInt();
-        System.out.println("ENter the second number: ");
+        System.out.println("Enter the second number: ");
         int num2 = sc.nextInt();
 
-        int result = (num1 & num2);
+        int result = (num1 | num2);
         System.out.println("Result is: "+result);
     }
 }
