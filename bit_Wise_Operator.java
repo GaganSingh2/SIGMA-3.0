15fec009a79a5871bec54f42e2c6d9f6deec253b
GaganSingh2 <gks022004@gmail.com>
2024-08-23 15:28:07 +0530

    "Binary Left Shift"

--- bit_Wise_Operator.java
+++ bit_Wise_Operator.java
@@ -33,12 +33,34 @@ public class bit_Wise_Operator {
         //-----------BINARY XOR(^)----------
         /* Each bit in the result is set to 1 if the corresponding bits in the input numbers are different. */
 
-        System.out.println("ENter the first number: ");
-        int num1 = sc.nextInt();
-        System.out.println("Enter the second number: ");
-        int num2 = sc.nextInt();
+        // System.out.println("ENter the first number: ");
+        // int num1 = sc.nextInt();
+        // System.out.println("Enter the second number: ");
+        // int num2 = sc.nextInt();
 
-        int result = (num1^num2);
+        // int result = (num1^num2);
+        // System.out.println("Result is: "+result);
+
+
+        //----------BINARY ONE's COMPLEMENT----------
+        /*Binary One's Complement is a bitwise operation that inverts all the bits of a binary number. It essentially changes all 0s to 1s and all 1s to 0s. */
+
+        // System.out.println("Enter first number: ");
+        // int num1 = sc.nextInt();
+
+        // int result = (~num1);
+        // System.out.println("Result is: "+result);
+
+
+        //----------BINARY LEFT SHIFT(<<)---------
+        /*Binary Left Shift is a bitwise operation that shifts all bits of a number to the left by a specified number of positions. The vacated positions on the right are filled with 0s. ex:- a<<b */
+
+        System.out.println("Enter the value of A: ");
+        int a = sc.nextInt();
+        System.out.println("Enter the value of B: ");
+        int b = sc.nextInt();
+
+        int result = (a<<b);
         System.out.println("Result is: "+result);
     }
 }
