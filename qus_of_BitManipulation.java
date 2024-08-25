8207e37ff923cf99c5330cdd3172e7e98d37da4c
GaganSingh2 <gks022004@gmail.com>
2024-08-26 01:16:35 +0530

    "Power of 2 or not"

--- qus_of_BitManipulation.java
+++ qus_of_BitManipulation.java
@@ -11,12 +11,34 @@ public class qus_of_BitManipulation {
             System.out.println(n+" is Odd Number.");
         }
     }
+
+    public static boolean isPower(int num){
+        boolean res = false;
+        if ((num & (num-1))==0) {
+            res = true;
+        }
+        else{
+            res = false;
+        }
+        return res;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         //Q1)Find tthe given number is Even or Odd.
-        System.out.println("Enter the value of N: "); 
-        int n = sc.nextInt();
-        evenOrOdd(n);
+        // System.out.println("Enter the value of N: "); 
+        // int n = sc.nextInt();
+        // evenOrOdd(n);
+
+        //Q2) Check if a number is Power of 2 or not.
+        System.out.println("Enter the number: ");
+        int num = sc.nextInt();
+        boolean result = isPower(num);
+        if (result==true) {
+            System.out.println(num+" is Power of 2.");
+        }
+        else{
+            System.out.println(num+" is not Power of 2.");
+        }
     }
 }
