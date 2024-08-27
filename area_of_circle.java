f3458dda3cd6f55d6c81fd95fca81b3e73cd44a6
GaganSingh2 <gks022004@gmail.com>
2024-08-28 02:00:37 +0530

    "point"

--- area_of_circle.java
+++ area_of_circle.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class area_of_circle {
+public class Area_of_circle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
