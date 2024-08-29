4f676cebf8db883b634db5f64056f99535393794
GaganSingh2 <gks022004@gmail.com>
2024-08-30 01:45:00 +0530

    "Multilevel Inheritance"

--- Inheritance.java
+++ Inheritance.java
@@ -5,9 +5,16 @@ public class Inheritance {
         shark.eat();
         shark.breath();
         shark.swim();
+
+        //(2)MultiLevel Inheritance ex ka object hai ye
+        Nano neo = new Nano();
+        neo.run();
+        neo.space();
+        neo.wheel();
     }
 }
 
+//Structure of Inheritance
 //Base class (Parent Class or Super class)
 class Animal{
     String color;
@@ -24,5 +31,33 @@ class Animal{
 class Fish extends Animal{
     void swim(){
         System.out.println("Swim");
+    }
+}
+
+//Types of Inheritance
+//(1)Single Inheritance:- A subclass inherits from only one superclass. example is the same as Inheritance
+
+//(2)Multilevel Inheritance:- A subclass inherits from another subclass, forming a chain of inherits.
+//Base CLass
+class Car{
+    String color;
+
+    void run(){
+        System.out.println("Fast");
+    }
+    void space(){
+        System.out.println("Big");
+    }
+}
+//Subclass1 inherit from base class
+class BMW extends Car{
+    void wheel(){
+        System.out.println("Four");
+    }
+}
+//Subclass2 inherit from subclass1
+class Nano extends BMW{
+    void size(){
+        System.out.println("Small");
     }
 }
