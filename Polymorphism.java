34b0b4dabf9fc217d4bc556e6db8ad8871c60728
GaganSingh2 <gks022004@gmail.com>
2024-09-02 00:42:00 +0530

    "Method Overriding"

--- Polymorphism.java
+++ Polymorphism.java
@@ -5,9 +5,16 @@ public class Polymorphism {
         System.out.println("Sum: "+calc.sum(4, 5));
         System.out.println("Sum: "+calc.sum((float)4.5, (float)5.5));
         System.out.println("Sum: "+calc.sum(4, 5, 10));
+
+        //------METHOD OVERRIDING------
+        Deer d = new Deer();
+        d.eat();
+        Cat c = new Cat();
+        c.eat();
     }
 }
 
+//--Method Overloading:-Multiple function with the same name but differnce parameter
 class Calculator{
     int sum(int a, int b){
         return a+b;
@@ -17,5 +24,22 @@ class Calculator{
     }
     int sum(int a, int b, int c){
         return a+b+c;
+    }
+}
+
+//---Method Overriding:-Parent and child class both contain the samme function with a diff. definition.
+class Animal{
+    void eat(){
+        System.out.println("Eats Everything.");
+    }
+}
+class Deer extends Animal{
+    void eat(){
+        System.out.println("Eat grass.");
+    }
+}
+class Cat extends Deer{
+    void eat(){
+        System.out.println("Eats Chicken.");
     }
 }
