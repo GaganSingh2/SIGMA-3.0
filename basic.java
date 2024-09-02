a114c547f2952b71f02dac7f50ebb952b2a06cf1
GaganSingh2 <gks022004@gmail.com>
2024-09-03 01:47:59 +0530

    "Some  points"

--- basic.java
+++ basic.java
@@ -1,8 +1,22 @@
 public class basic {
-    public static void main(String[] args) {
-        for(int i=0; i<=10; i++){
-            System.out.println(i);
+    class Solution {
+        public int[] twoSum(int nums[], int target) {
+            int ans[] = new int[2];
+            for(int i=0; i<nums.length-1; i++){
+                for(int j=1; j<nums.length; j++){
+                    if(nums[i]+nums[j]==target){
+                        return new int[]{i, j};
+                    }
+                }
+            }
+            return new int[]{};//for no solution found
         }
-        System.out.println(1%2);
     }
-}
+    public static void main(String[] args) {
+        // for(int i=0; i<=10; i++){
+        //     System.out.println(i);
+        // }
+        // System.out.println(1%2);
+        
+    }
+} 
