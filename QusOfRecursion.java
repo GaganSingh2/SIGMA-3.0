be6d3c2a61dd99f759ba5851ee0f4dd71df3fcd1
GaganSingh2 <gks022004@gmail.com>
2024-09-08 03:02:43 +0530

    "Add another question"

--- QusOfRecursion.java
+++ QusOfRecursion.java
@@ -9,11 +9,25 @@ public class QusOfRecursion {
         System.out.print(num+" ");
         printDec(num-1);
     }
+
+    public static void printInc(int num){
+        if (num==1) {
+            System.out.print(num+" ");
+            return;
+        }
+        printInc(num-1);
+        System.out.print(num+" ");
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         //Q1) Print numbers from n to 1 (Decreasing order).
-        System.out.println("Enter Max value: ");
+        // System.out.println("Enter Max value: ");
+        // int num = sc.nextInt();
+        // printDec(num);
+
+        //Q2)Print numbers from n to 1(Increasing order)
+        System.out.println("Enter Number: ");
         int num = sc.nextInt();
-        printDec(num);
+        printInc(num);
     }
 }
