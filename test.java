984ca66a91256bdc8c4bf001bc06c10f23571b01
GaganSingh2 <gks022004@gmail.com>
2024-10-01 22:06:23 +0530

    "another interesting qus"

--- test.java
+++ test.java
@@ -23,13 +23,39 @@ public class test {
         System.out.println();
         
     }
+
+    public static boolean canArrange(int[] arr, int k) {
+        int [] freq = new int[k];
+        for(int num: arr){
+            int rem = num % k;
+            if(rem < 0){
+                rem = rem + k;
+            }
+            freq[rem]++;
+        }
+        if(freq[0] % 2 != 0){
+            return false;
+        }
+        for(int i = 1;i <= k / 2;i++){
+            if(freq[i] != freq[k-i]) {
+                return false;
+            }
+        }
+        return true;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int arr[] = {5,4,1,3,2};
-        insertionSort(arr);
-        printArr(arr);
+        // int arr[] = {5,4,1,3,2};
+        // insertionSort(arr);
+        // printArr(arr);
        
 
-        //Q2) CHeck the given integer is palindrome or not
+        //Q2)[1497]Check if array pairs are divisible by K.
+        int arr[]={1,2,3,4,5,10,6,7,8,9};
+        System.out.println("Enteer the value of K: ");
+        int k = sc.nextInt();
+        System.out.println(canArrange(arr, k)); 
+            
+
     }
 }
