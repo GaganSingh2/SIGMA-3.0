0bfeb557cdd54cd197d9992ca51f8b79d5b69198
GaganSingh2 <gks022004@gmail.com>
2024-11-04 22:49:40 +0530

    "Creation of Array_List"

---
+++ Array_List.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+public class Array_List {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //Syntax of ArrayList
+        ArrayList<Integer> list1 = new ArrayList<>(); //int type arraylist
+        ArrayList<String> list2 = new ArrayList<>(); //String type arraylist
+        ArrayList<Boolean> list3 = new ArrayList<>(); //Boolean type arraylist
+    }
+}
