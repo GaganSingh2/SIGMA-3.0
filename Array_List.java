9f8b8a4cead15b10e55c0bdccb3d34b2cb87db94
GaganSingh2 <gks022004@gmail.com>
2024-11-05 11:53:58 +0530

    "Operation On ArrayList"

--- Array_List.java
+++ Array_List.java
@@ -8,5 +8,48 @@ public class Array_List {
         ArrayList<Integer> list1 = new ArrayList<>(); //int type arraylist
         ArrayList<String> list2 = new ArrayList<>(); //String type arraylist
         ArrayList<Boolean> list3 = new ArrayList<>(); //Boolean type arraylist
+
+        //---Operation on ArrayList
+        //1) Adding element
+        System.out.println("Adding Element:");
+        list2.add("orange");
+        list2.add("Apple");
+        list2.add(0,"Grapes");
+        System.out.println(list2);
+
+        //2) Get Element
+        System.out.println("Accessing Element:");
+        String res = list2.get(1);
+        System.out.println(res);
+
+        //3) Remove Element
+        System.out.println("Removing Element: ");
+        list2.remove(1); //remove element through index numbe
+        // list2.remove("Apple"); //remove element through element name
+        System.out.println(list2);
+
+        //4) Modifying Element
+        System.out.println("Modifying Element: ");
+        list2.set(1, "Banana");
+        System.out.println(list2);
+
+        //5)Checking for an Element
+        System.out.println("Checking for an Element: ");
+        System.out.println(list2.contains("Apple"));
+        System.out.println(list2.contains("Banana"));
+        
+        //6)Size of ArrayList
+        System.out.println("Size of ArrayList: ");
+        int size = list2.size();
+        System.out.println(size);
+
+        //7)Clearing the ArrayList:-Remove all element in ArrayList
+        System.out.println("Clearing all Element: ");
+        list2.clear();
+        System.out.println(list2);
+
+        //8)Checking ArrayList is isEmpty or not
+        System.out.println("Checking is isEmpty or not: ");
+        System.out.println(list2.isEmpty());
     }
 }
