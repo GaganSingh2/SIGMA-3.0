b5e8552b46a93bc53c716ed2a66381ddfb8f1c4d
GaganSingh2 <gks022004@gmail.com>
2024-11-06 11:01:22 +0530

    "Sorting with Collections method"

--- Qus_of_Array_List.java
+++ Qus_of_Array_List.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class Qus_of_Array_List {
      public static void swap(ArrayList<Integer>list, int idx1,int idx2){
@@ -35,10 +37,19 @@ public class Qus_of_Array_List {
           // System.out.println("Maximum Element: "+max);
 
           //Q3)Swap the values
-          int idx1=1, idx2=3;
-          System.out.println("Before Swapping: "+list);
-          swap(list, idx1, idx2);
-          System.out.println("After Swapping: "+list);
+          // int idx1=1, idx2=3;
+          // System.out.println("Before Swapping: "+list);
+          // swap(list, idx1, idx2);
+          // System.out.println("After Swapping: "+list);
+
+          //Q4)Sorting in ArrayList
+          System.out.println("Before Sorting in Ascending order: "+list);
+          Collections.sort(list);
+          System.out.println("After Sorting in Ascending order: "+list);
+
+          System.out.println("Before Sorting in Descending order: "+list);
+          Collections.sort(list,Collections.reverseOrder());
+          System.out.println("After Sorting in Dscending order: "+list);
 
      }
 }
