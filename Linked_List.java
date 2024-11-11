29109a1b83902c26135e6e7a36e1d3643a6c2ec4
GaganSingh2 <gks022004@gmail.com>
2024-11-12 00:16:14 +0530

    "Concept of adding the node in last position"

--- Linked_List.java
+++ Linked_List.java
@@ -27,10 +27,26 @@ public class Linked_List {
         //step3-> head = newNode
         head = newNode;
     }
+    //Adding the newNode in last position
+    public void addLast(int data){
+        //step1-> create new Node
+        Node newNode = new Node(data);
+        if (head==null) {
+            head = tail = newNode;
+            return;
+        }
+        //step2-> link the previous node to newNode
+        tail.next = newNode;
+
+        //step3-> tail = newNode
+        tail = newNode;
+    }
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
         ll.addFirst(1);
         ll.addFirst(2);
+        ll.addLast(3);
+        ll.addLast(4);
         
     }
 }
