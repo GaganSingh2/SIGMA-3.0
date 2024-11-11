e74d795b7f72ec3b86617d9d6c5000b0cb649d32
GaganSingh2 <gks022004@gmail.com>
2024-11-12 00:03:26 +0530

    "Add new Node in first position"

--- Linked_List.java
+++ Linked_List.java
@@ -12,7 +12,25 @@ public class Linked_List {
     }
     public static Node head;
     public static Node tail;
+
+    //Adding the new Node in first position
+    public void addFirst(int data){
+        //step1= create new Node
+        Node newNode = new Node(data);
+        if (head==null) {
+            head = tail = newNode;
+            return;
+        }
+        //step2-> link the newNode to head
+        newNode.next = head; 
+
+        //step3-> head = newNode
+        head = newNode;
+    }
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
+        ll.addFirst(1);
+        ll.addFirst(2);
+        
     }
 }
