6816ea89c495fc75a4d9c2b2c6c2e908ba9b8756
GaganSingh2 <gks022004@gmail.com>
2024-11-16 19:43:20 +0530

    "Using recursion"

--- Qus_Of_LinkedList.java
+++ Qus_Of_LinkedList.java
@@ -33,6 +33,7 @@ public class Qus_Of_LinkedList {
        }
        System.out.println("null");
     }
+
     public static int searchKey(int key){
         Node temp = head;
         int idx = 0;
@@ -48,6 +49,25 @@ public class Qus_Of_LinkedList {
         }
         return -1;
     }
+
+    //Using Recursion
+    public int helper(int key, Node head){
+        //Base case
+        if (head == null) {
+            return -1;
+        }
+        if (head.data == key) {
+            return 0;
+        }
+        int idx = helper(key, head.next);
+        if (idx == -1) {
+            return -1;
+        }
+        return idx+1;
+    }
+    public  int recSearch(int key){
+        return helper(key,head);
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -59,10 +79,15 @@ public class Qus_Of_LinkedList {
         linkqus.addFirst(5);
 
         linkqus.print();
+        //Q1)Search the key and return the position of key using loop
         System.out.println("Enter the key: ");
         int key = sc.nextInt();
 
-        int res = linkqus.searchKey(key);
-        System.out.println("Postion of Node: "+res);
+        // int res = linkqus.searchKey(key);
+        // System.out.println("Postion of Node: "+res);
+
+        //Same qus using Recursion
+        int result = linkqus.recSearch(key);
+        System.out.println("Position of Node: "+result);
     }
 }
