804524bebaa9ece51c075d4122c1a13e508fbc75
GaganSingh2 <gks022004@gmail.com>
2024-11-27 23:25:48 +0530

    "Understand LinkedList using Framework,Interface and classes"

---
+++ LinkedListUsing_Framework.java
@@ -0,0 +1,21 @@
+import java.util.LinkedList;
+
+public class LinkedListUsing_Framework {
+    public static void main(String[] args) {
+        //Create a Linked List
+        LinkedList<Integer> link = new LinkedList<>();
+
+        //Add the node 
+        link.addFirst(1);
+        link.addFirst(2);
+        link.addLast(5);
+        System.out.println("LinkedList: "+link);
+        //remove the Node
+        link.removeFirst();
+        link.removeLast();
+
+        //print
+        System.out.println("After Deletion: "+link);
+
+    }
+}
