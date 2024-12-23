8980029fc6fdb193f99c9666a05730a71bcc56bb
GaganSingh2 <gks022004@gmail.com>
2024-12-23 20:52:28 +0530

    "array inital"

--- Stack.java
+++ Stack.java
@@ -3,5 +3,16 @@ import java.util.Scanner;
 public class Stack {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
+        System.out.println("enter the length: ");
+        int len = sc.nextInt();
+        int arr[] = new int[len];
+
+        System.out.println("Enter Element: ");
+        for(int i=0; i<len; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("ArraY: ");
+        
     }
 }
