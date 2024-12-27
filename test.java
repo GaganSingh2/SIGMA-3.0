e466f7cd14b9bcaf65bcd789ba52f8a2c1716485
GaganSingh2 <gks022004@gmail.com>
2024-12-27 23:25:40 +0530

    "Array is SOrted or not"

--- test.java
+++ test.java
@@ -315,6 +315,15 @@ public class test {
         }
         return false;
     }
+
+    public static int isSorted(int arr[]){
+        for(int i=1; i<arr.length; i++){
+            if(arr[i]<arr[i-1]){
+                return 0;
+            }
+        }
+        return 1;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         // int arr[] = {5,4,1,3,2};
@@ -547,12 +556,28 @@ public class test {
          //Space Complexity: O(1)
 
          //Q) Check the given string is periodic or not(Periodic means the given string can repeate less then time of our size)
-         System.out.println("ENter the String: ");
-         String str = new String();
-         Boolean res = isPeriodic(str);
-         System.out.println("Result: "+res);
+        //  System.out.println("ENter the String: ");
+        //  String str = new String();
+        //  Boolean res = isPeriodic(str);
+        //  System.out.println("Result: "+res);
 
          //Time Complexity: O(n)
          //Space Complexity: O(1)
+
+         //Q)You have been given an array ‘a’ of ‘n’ non-negative integers.You have to check whether the given array is sorted in the non-decreasing order or not.Your task is to return 1 if the given array is sorted. Else, return 0.
+         System.out.println("ENter the lenght: ");
+         int len = sc.nextInt();
+         int arr[] = new int[len];
+         System.out.println("Enter the value in Array: ");
+         for(int i=0; i<len; i++){
+            arr[i] = sc.nextInt();
+         }
+         System.out.println("Array: ");
+         for(int i=0; i<len; i++){
+            System.out.print(arr[i]+" ");
+         }System.out.println();
+
+         int res = isSorted(arr);
+         System.out.println("Result is: "+res);
     }
 }
