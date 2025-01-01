1391775c406ab421b8e31d93deb7b20d84babb9e
GaganSingh2 <gks022004@gmail.com>
2025-01-01 23:22:57 +0530

    "input"

--- input.java
+++ input.java
@@ -22,6 +22,18 @@ public class input {
         
         System.out.println("Your choice is "+bool);
 
+        System.out.println("Enter the len: ");
+        int len = sc.nextInt();
         
+        int arr[] = new int[len];
+
+        System.out.println("Enter the value: ");
+        for(int i=0; i<len; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Array: ");
+        for(int i=0; i<len; i++){
+            System.out.print(arr[i]+" ");
+        }
     }
 }
