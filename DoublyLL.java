dd404e5c024686cac476f042343e527e64fb87ad
GaganSingh2 <gks022004@gmail.com>
2025-01-22 10:57:24 +0530

    "Reverse the DLL"

--- DoublyLL.java
+++ DoublyLL.java
@@ -1,24 +1,25 @@
 public class DoublyLL {
-    public class Node{
+    public class Node {
         int data;
         Node next;
         Node prev;
-        
-        public Node(int data){
+
+        public Node(int data) {
             this.data = data;
             this.next = null;
             this.prev = null;
         }
     }
+
     public static Node head;
     public static Node tail;
     public static int size;
 
-    //AddFirst Method
-    public void addFirst(int data){
+    // AddFirst Method
+    public void addFirst(int data) {
         Node newNode = new Node(data);
         size++;
-        if(head == null){
+        if (head == null) {
             head = tail = newNode;
             return;
         }
@@ -27,18 +28,18 @@ public class DoublyLL {
         head = newNode;
     }
 
-    //Remove First
-    public int removeFirst(){
+    // Remove First
+    public int removeFirst() {
         int val = Integer.MIN_VALUE;
         if (head == null) {
             System.out.println("DLL is Empty");
             return val;
         }
-        
-        if (size==1) {
+
+        if (size == 1) {
             val = head.data;
             head = tail = null;
-            size=0;
+            size = 0;
             return val;
         }
         val = head.data;
@@ -47,9 +48,9 @@ public class DoublyLL {
         size--;
         return val;
     }
-    
-    //AddLast
-     public void addLast(int data){
+
+    // AddLast
+    public void addLast(int data) {
         Node newNode = new Node(data);
         size++;
         if (tail == null) {
@@ -59,17 +60,17 @@ public class DoublyLL {
         newNode.prev = tail;
         tail.next = newNode;
         tail = newNode;
-     }
+    }
 
-     //RemoveLast
-     public int removeLast(){
+    // RemoveLast
+    public int removeLast() {
         int val = Integer.MIN_VALUE;
-        //if DLL is empty
-        if(tail == null) {
+        // if DLL is empty
+        if (tail == null) {
             System.out.println("DLL is Empty.");
             return val;
         }
-        if(size==1){
+        if (size == 1) {
             val = tail.data;
             head = tail = null;
             size = 0;
@@ -80,69 +81,70 @@ public class DoublyLL {
         tail.next = null;
         size--;
         return val;
-     }
+    }
 
-     //Reverse the DLL
-     public void reverse(){
+    // Reverse the DLL
+    public void reverse() {
         Node curr = head;
-        Node prev = null;
+        Node prev = null; 
         Node next;
-        while (curr != null) {
+        while(curr != null){
             next = curr.next;
             curr.next = prev;
-            curr.prev = next; //Additional line 
+            curr.prev = next;
 
             prev = curr;
             curr = next;
         }
         head = prev;
-     }
-    //Print the LL
-    public void print(){
+    }
+
+    // Print the LL
+    public void print() {
         Node temp = head;
-        
+
         System.out.print("null<-->");
         while (temp != null) {
-            System.out.print(temp.data+"<-->");
+            System.out.print(temp.data + "<-->");
             temp = temp.next;
         }
-        System.out.println("null"); 
+        System.out.println("null");
     }
+
     public static void main(String[] args) {
         DoublyLL dll = new DoublyLL();
 
-        //AddFirst
+        // AddFirst
         dll.addFirst(3);
         dll.addFirst(2);
         dll.addFirst(1);
 
-        //Print
-        //dll.print();
-        //System.out.println("Size of DLL: "+size);
+        // Print
+        // dll.print();
+        // System.out.println("Size of DLL: "+size);
 
-        //Remove First
+        // Remove First
         // dll.removeFirst();
         // dll.print();
         // System.out.println("Size of DLL: "+size);
 
-        //AddLast
+        // AddLast
         dll.print();
         dll.addLast(4);
         dll.addLast(5);
         dll.addLast(6);
         dll.print();
-        System.out.println("Size Of DLL: "+size);
+        System.out.println("Size Of DLL: " + size);
 
-        //Remove Last
+        // Remove Last
         // dll.removeLast();
         // dll.removeLast();
         // dll.print();
         // System.out.println("Size of DLL: "+size);
 
-        //Reverse the DLL
+        // Reverse the DLL
         dll.reverse();
         dll.print();
 
-        
     }
 }
