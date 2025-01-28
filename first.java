27461185d15e9e14331216a3482976c4d0b73539
GaganSingh2 <gks022004@gmail.com>
2025-01-28 22:30:04 +0530

    "helo"

--- first.java
+++ first.java
@@ -1,6 +1,8 @@
 public class first {
         public static void main(String[] args) {
             System.out.println("Radhe Radhe!");
+
+            System.out.println("Sab ko sab nhi milta");
         }
     
 }
