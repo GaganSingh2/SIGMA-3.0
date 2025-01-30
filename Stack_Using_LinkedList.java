4045240e07ca293750c85bf26c06f3fbaa49b750
GaganSingh2 <gks022004@gmail.com>
2025-01-30 21:02:53 +0530

    "Stack using LinkedList"

--- Stack_Using_LinkedList.java
+++ Stack_Using_LinkedList.java
@@ -15,8 +15,45 @@ public class Stack_Using_LinkedList {
         public static boolean isEmpty(){
             return head == null;
         }
+
+        //For Push
+        public static void push(int data){
+            Node newNode = new Node(data);
+            if (isEmpty()){
+                head = newNode;
+                return;
+            }
+            newNode.next = head;
+            head = newNode;
+        }
+
+        //For Pop
+        public static int pop(){
+            if (isEmpty()) {
+                return -1;
+            }
+            int top_Data = head.data;
+            head = head.next;
+            return top_Data;
+        }
+
+        //For peek or Top 
+        public static int peek(){
+            if (isEmpty()) {
+                return -1;
+            }
+            return head.data;
+        }
     }
     public static void main(String[] args) {
-        
+        Stack sb = new Stack();
+        sb.push(1);
+        sb.push(2);
+        sb.push(3);
+
+        while (!sb.isEmpty()) {
+            System.out.println(sb.peek());
+            sb.pop();
+        }
     }
 }
