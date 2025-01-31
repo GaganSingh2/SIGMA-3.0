ab2d2911b78cb66cbbe6e49b8c57c7dc1b99c7e2
GaganSingh2 <gks022004@gmail.com>
2025-01-31 20:55:22 +0530

    "Push the value at the Bottom"

--- Stack_Using_CollectionsFramework.java
+++ Stack_Using_CollectionsFramework.java
@@ -2,13 +2,29 @@ import java.util.*;
 import java.util.Stack;
 
 public class Stack_Using_CollectionsFramework{
+    public static void pushAtBottom(Stack<Integer> sb,int data){
+        if (sb.isEmpty()) {
+            sb.push(data);
+            return;
+        }
+        int top = sb.pop();
+        pushAtBottom(sb, data);
+        sb.push(top);
+    }
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Stack<Integer> list = new Stack<>();
 
         list.push(1);
         list.push(2);
         list.push(3);
 
+        //Q1) Push the data at the bottom of the stack
+        System.out.print("Enter the Data: ");
+        int data = sc.nextInt();
+        pushAtBottom(list, data);
+
+        System.out.println("After the Pushing the DAta At Bottom: ");
         while (!list.isEmpty()) {
             System.out.println(list.peek());
             list.pop();
