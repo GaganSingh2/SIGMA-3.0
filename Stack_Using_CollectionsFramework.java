95486b27ccb4c9f53c35b4b461d39d93b13c8b1d
GaganSingh2 <gks022004@gmail.com>
2025-01-31 22:07:45 +0530

    "REverse the String using stack"

--- Stack_Using_CollectionsFramework.java
+++ Stack_Using_CollectionsFramework.java
@@ -7,9 +7,26 @@ public class Stack_Using_CollectionsFramework{
             sb.push(data);
             return;
         }
-        int top = sb.pop();
+        int top = sb.pop(); 
         pushAtBottom(sb, data);
         sb.push(top);
+    }
+
+    public static String reverseOfString(String letter){
+        Stack<Character> ch = new Stack<>();
+        int idx = 0;
+        while (idx < letter.length()) {
+            ch.push(letter.charAt(idx));
+            idx++;
+        }
+
+        StringBuilder sb = new StringBuilder("");
+        while (!ch.isEmpty()) {
+            char curr = ch.pop();
+            sb.append(curr);
+        }
+        return sb.toString();
+
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -20,15 +37,21 @@ public class Stack_Using_CollectionsFramework{
         list.push(3);
 
         //Q1) Push the data at the bottom of the stack
-        System.out.print("Enter the Data: ");
-        int data = sc.nextInt();
-        pushAtBottom(list, data);
+        // System.out.print("Enter the Data: ");
+        // int data = sc.nextInt();
+        // pushAtBottom(list, data);
 
-        System.out.println("After the Pushing the DAta At Bottom: ");
-        while (!list.isEmpty()) {
-            System.out.println(list.peek());
-            list.pop();
-        }
+        // System.out.println("After the Pushing the DAta At Bottom: ");
+        // while (!list.isEmpty()) {
+        //     System.out.println(list.peek());
+        //     list.pop();
+        // }
 
+        //Q2) Reverse the String using Stack
+        
+        System.out.println("Enter the String: ");
+        String letter = sc.nextLine();
+        String result = reverseOfString(letter);
+        System.out.println("After Reversing the String: "+result);
     }
 }
