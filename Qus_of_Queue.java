041501a8a4dc921b4b067fd6d26174d5313210af
GaganSingh2 <gks022004@gmail.com>
2025-03-11 22:32:58 +0530

    "2nd Way"

--- Qus_of_Queue.java
+++ Qus_of_Queue.java
@@ -6,45 +6,88 @@ public class Qus_of_Queue {
         static Stack<Integer> list1 = new Stack<>();
         static Stack<Integer> list2 = new Stack<>();
 
+        // public static boolean isEmpty(){
+        //     return list1.isEmpty();
+        // }
+
+        // // Add - O(n)
+        // public static void add(int data){
+        //    while (!list1.isEmpty()) {
+        //         list2.push(list1.pop());
+        //    }
+        //    list1.push(data);
+        //    while (!list2.isEmpty()) {
+        //         list1.push(list2.pop());
+        //    }
+        // }
+
+        // //Remove - O(1)
+        // public static int remove(){
+        //     if(isEmpty()){
+        //         System.out.println("Queue is Empty.");
+        //         return -1;
+        //     }
+        //     return list1.pop();
+        // }
+
+        // // Peek - O(1)
+        // public static int peek(){
+        //     if (isEmpty()) {
+        //         System.out.println("Queue is Empty");
+        //         return -1;
+        //     }
+        //     return list1.peek();
+        // }
+
+        // 2nd Way-------
+
         public static boolean isEmpty(){
-            return list1.isEmpty();
+            return list1.isEmpty() && list2.isEmpty();
         }
-
-        // Add - O(n)
+        
+        //Add
         public static void add(int data){
-           while (!list1.isEmpty()) {
-                list2.push(list1.pop());
-           }
-           list1.push(data);
-           while (!list2.isEmpty()) {
-                list1.push(list2.pop());
-           }
+            list1.push(data);
         }
 
-        //Remove - O(1)
+        // Remove (pop)
         public static int remove(){
-            if(isEmpty()){
-                System.out.println("Queue is Empty.");
-                return -1;
-            }
-            return list1.pop();
+            // if(list1.isEmpty() && list2.isEmpty()){
+            //     System.out.println("Queue is Empty");
+            //     return -1;
+            // }
+            // // if list2 is empty but list1 have some data
+            // if(list2.isEmpty()){
+            //     while (!list1.isEmpty()) {
+            //         list2.push(list1.pop());
+            //     }
+            // }
+            peek();
+            return list2.pop();
         }
-
-        // Peek - O(1)
+        
+        // Peek
         public static int peek(){
-            if (isEmpty()) {
+            if(list1.isEmpty() && list2.isEmpty()){
                 System.out.println("Queue is Empty");
                 return -1;
             }
-            return list1.peek();
+            // if list2 is empty but list1 have some data
+            if(list2.isEmpty()){
+                while (!list1.isEmpty()) {
+                    list2.push(list1.pop());
+                }
+            }
+            return list2.peek();
         }
     }
     public static void main(String[] args) {
         // Q1)Implement Queue using 2 stack
         Queue val = new Queue();
-        val.add(1);
+        val.add(1);  
         val.add(2);
         val.add(3);
+        
 
         while (!val.isEmpty()) {
             System.out.println(val.peek());
