80a0bdf30b4a3e85bf2dff12c19d3f0545103d7e
GaganSingh2 <gks022004@gmail.com>
2025-03-26 23:00:29 +0530

    "Implement Stack using Deque"

---
+++ Deque_Using_Stack.java
@@ -0,0 +1,32 @@
+import java.util.Stack;
+import java.util.Deque;
+import java.util.LinkedList;
+public class Deque_Using_Stack {
+    static class Stack{
+        Deque<Integer> list = new LinkedList<>();
+        public void push(int data){
+            list.addLast(data);
+        }
+
+        public int pop(){
+            return list.removeLast();
+        }
+
+        public int peek(){
+            return list.getLast();
+        }
+    }
+    public static void main(String[] args) {
+        Stack temp = new Stack();
+
+        temp.push(1);
+        temp.push(2);
+        temp.push(3);
+
+        System.out.println("Peek of the value in Deque: "+temp.peek());
+        System.out.println("value is: "+temp.pop());
+        System.out.println("value is: "+temp.pop());
+        System.out.println("value is: "+temp.pop());
+        
+    }
+}
