f134b865d8941f9672806b63be648b76ffac2fe9
GaganSingh2 <gks022004@gmail.com>
2025-04-09 02:23:11 +0530

    "If EndTime is not sorted"

--- Greedy_Algo.java
+++ Greedy_Algo.java
@@ -22,19 +22,20 @@ public class Greedy_Algo {
 
         //if endTime is not sorted then
         int activities[][] = new int[startTime.length][3];
-        for(int i=0; i<activities.length; i++){
+        for(int i=0; i<startTime.length; i++){
             activities[i][0] = i;
             activities[i][1] = startTime[i];
             activities[i][2] =endTime[i];
         }
 
-        // it is sorting according to the our condition
+        // it is sorting according to the our condition using Lambda function
         Arrays.sort(activities, Comparator.comparingDouble(o -> o[2]));
+
         maxAct = 1;
         ActPerform.add(activities[0][0]);
         int lastEnd = activities[0][2];
 
-        for(int i=1; i<activities.length; i++){
+        for(int i=1; i<endTime.length; i++){
             if(activities[i][1]>=lastEnd){
                 maxAct++;
                 ActPerform.add(activities[i][0]);
@@ -43,7 +44,7 @@ public class Greedy_Algo {
         }
 
         System.out.println("Maximum Number of Activity Perform: "+maxAct);
-        
+
         System.out.print("Activities: ");
         for(int i=0; i<ActPerform.size(); i++){
             System.out.print("A"+ActPerform.get(i)+" ");
@@ -51,8 +52,8 @@ public class Greedy_Algo {
     }
     public static void main(String[] args) {
         // Q1)--Activity Selection--. you are given n activities with their start and end time. select the maximum number of activity that can be perform by a single person, assuming that a person can only work on a single activity at a time. Activities are sorted according to end time.
-        int startTime[] = {1,3,0,5,8,5};
-        int endTime[] = {2,4,6,7,9,9};
+        int startTime[] = {0,1,3,5,5,8};
+        int endTime[] = {6,2,4,7,9,9};
         
         max_Activity_Selection(startTime, endTime);
 
