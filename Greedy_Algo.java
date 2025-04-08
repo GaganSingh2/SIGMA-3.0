33857e103fbe9b596fca2d2e7a3c4ed2cbc6dd1f
GaganSingh2 <gks022004@gmail.com>
2025-04-09 00:53:35 +0530

    "Select Activity if EndTime is not Sorted"

--- Greedy_Algo.java
+++ Greedy_Algo.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class Greedy_Algo {
     public static void max_Activity_Selection(int startTime[], int endTime[]){
@@ -6,19 +8,42 @@ public class Greedy_Algo {
         ArrayList<Integer> ActPerform = new ArrayList<>();
 
         //if endTime is sorted
-        maxAct = 1;
-        ActPerform.add(0);
-        int lastTime = endTime[0];
+        // maxAct = 1;
+        // ActPerform.add(0);
+        // int lastTime = endTime[0];
 
-        for(int i=1; i<endTime.length; i++){
-            if (startTime[i]>=lastTime) {
+        // for(int i=1; i<endTime.length; i++){
+        //     if (startTime[i]>=lastTime) {
+        //         maxAct++;
+        //         ActPerform.add(i);
+        //         lastTime = endTime[i];
+        //     }
+        // }
+
+        //if endTime is not sorted then
+        int activities[][] = new int[startTime.length][3];
+        for(int i=0; i<activities.length; i++){
+            activities[i][0] = i;
+            activities[i][1] = startTime[i];
+            activities[i][2] =endTime[i];
+        }
+
+        // it is sorting according to the our condition
+        Arrays.sort(activities, Comparator.comparingDouble(o -> o[2]));
+        maxAct = 1;
+        ActPerform.add(activities[0][0]);
+        int lastEnd = activities[0][2];
+
+        for(int i=1; i<activities.length; i++){
+            if(activities[i][1]>=lastEnd){
                 maxAct++;
-                ActPerform.add(i);
-                lastTime = endTime[i];
+                ActPerform.add(activities[i][0]);
+                lastEnd = activities[i][2];
             }
         }
 
         System.out.println("Maximum Number of Activity Perform: "+maxAct);
+        
         System.out.print("Activities: ");
         for(int i=0; i<ActPerform.size(); i++){
             System.out.print("A"+ActPerform.get(i)+" ");
