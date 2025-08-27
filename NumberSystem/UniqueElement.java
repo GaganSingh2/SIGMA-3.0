92ec594e15a149d72d1414ee7ecd2bacc197011b
GaganSingh2 <gks022004@gmail.com>
2025-08-27 16:46:17 +0530

    "Add Points"

--- NumberSystem/UniqueElement.java
+++ NumberSystem/UniqueElement.java
@@ -9,11 +9,21 @@ public class UniqueElement {
         }
         return unique;
     }
+    /*
+     * This Program Is Only Good if our Whole Array contain Positive Number 
+     * Ex: IP-> 1 2 3 2 1, OP-> 3
+     * 
+     * This Program Is Only Good if our Whole Array contain Negative Number 
+     * Ex: IP-> -1 -2 -3 -2 -1, OP-> -3
+     * 
+     * This Program Is not Work Properly if our Whole Array contains contains the combination of Positive and Negatives values.
+     * Ex: IP-> -1 -2 3 -1 -2, OP-> 1(Wrong Ans)
+     */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the length: ");
         int size = sc.nextInt();
-        int arr[] = new int[size];
+        int arr[] = new int[size]; 
         System.out.println("Enter the value: ");
         for(int i=0; i<size; i++){
             arr[i] = sc.nextInt();
