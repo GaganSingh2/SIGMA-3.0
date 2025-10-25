9ef6c0d199990762d596ba7f3b27ed3acfd02ee0
GaganSingh2 <gks022004@gmail.com>
2025-10-25 10:40:50 +0530

    "Count Activity Selection if EndTime is not sorted"

--- GreedyAlgo/ActivitySelection.java
+++ GreedyAlgo/ActivitySelection.java
@@ -1,6 +1,8 @@
 package GreedyAlgo;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class ActivitySelection {
@@ -8,16 +10,44 @@ public class ActivitySelection {
         int maxAct = 0;
         ArrayList<Integer> idx = new ArrayList<>();
 
-        //If EndTime is Already Sorted
-        maxAct = 1;
-        idx.add(0);
-        int lastEnd = et[0];
+        //If EndTime is Already Sorted------
+        // maxAct = 1;
+        // idx.add(0);
+        // int lastEnd = et[0];
 
+        // for(int i=1; i<et.length; i++){
+        //     if(st[i]>=lastEnd){
+        //         maxAct++;
+        //         idx.add(i);
+        //         lastEnd = et[i];
+        //     }
+        // }
+        // System.out.println("Total Activity Selected: "+maxAct);
+        // for(int i=0; i<idx.size(); i++){
+        //     System.out.print("A"+idx.get(i)+" ");
+        // }
+
+        // If EndTime is Not Sorted
+        int task[][] = new int[st.length][3];
+        for(int i=0; i<et.length; i++){
+            task[i][0] = i;
+            task[i][1] = st[i];
+            task[i][2] = et[i];
+        }
+        Arrays.sort(task,Comparator.comparingDouble(o -> o[2]));
+        for(int i=0; i<et.length; i++){
+            // task[i][0] = i;
+            // task[i][1] = st[i];
+            System.out.println(task[i][2]+" ");
+        }
+        maxAct = 1;
+        idx.add(task[0][0]);
+        int lastEnd = task[0][2];
         for(int i=1; i<et.length; i++){
-            if(st[i]>=lastEnd){
+            if(task[i][1]>=lastEnd){
                 maxAct++;
-                idx.add(i);
-                lastEnd = et[i];
+                idx.add(task[i][0]);
+                lastEnd = task[i][2];
             }
         }
         System.out.println("Total Activity Selected: "+maxAct);
