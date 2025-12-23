6f92802984fa5120596310ed76ba1050bc28894a
GaganSingh2 <gks022004@gmail.com>
2025-12-24 00:03:19 +0530

    "Implement Stack using ArrayList"

--- Stack/StackUsingArrayList.java
+++ Stack/StackUsingArrayList.java
@@ -1,49 +1,48 @@
 import java.util.ArrayList;
 import java.util.Stack;
 class StackUsingArrayList {
-    static class Stack{
-        ArrayList<Integer> list = new ArrayList<>();
-        //For Empty of Stack
+    static class Stack {
+        static ArrayList<Integer> list = new ArrayList<>();
+
+        //Empty
         public boolean isEmpty(){
-            return list.size() == 0;
+            return list.size()==0;
+        }
+        
+        //Push the value
+        public void push(int val){
+            list.add(val);
         }
 
-        //for Push in Stack
-        public void push(int data){
-            list.add(data);
-        }
-
-        //for pop in Stack
+        //Pop the value
         public int pop(){
-            if(isEmpty()){
+            if(list.isEmpty()){
                 return -1;
             }
-            int top = list.get(list.size() - 1);
-            list.remove(list.size() - 1);
+            int top = list.get(list.size()-1);
+            list.remove(list.size()-1);
             return top;
         }
 
-        //for peek in Stack
+        //Peek the value
         public int peek(){
-            if (isEmpty()) {
+            if(list.isEmpty()){
                 return -1;
             }
             return list.get(list.size()-1);
         }
     }
     public static void main(String[] args) {
-        Stack temp = new Stack();
-        temp.push(10);
-        temp.push(20);
-        temp.push(30);
-        temp.push(40);
-        temp.push(50);
-        
-        while (!temp.isEmpty()) {
-            System.out.println(temp.peek());
-            temp.pop();
+        Stack stk = new Stack();
+        stk.push(3);
+        stk.push(43);
+        stk.push(23);
+        stk.push(2);
+        System.out.println("Top Value: "+stk.peek());
+        while (!stk.isEmpty()) {
+            System.out.println("Top Value: "+stk.peek());
+            stk.pop();
         }
-        System.out.println("Stack is Empty: "+temp.peek());
-
+        System.out.println("Top Value: "+stk.peek());
     }
 }
