dcd603ed24b81b89ef4525528855690a5b1e3da2
GaganSingh2 <gks022004@gmail.com>
2025-12-24 00:25:33 +0530

    "Implement Stack Using LinkedList"

--- Stack/StackUsingLinkedList.java
+++ Stack/StackUsingLinkedList.java
@@ -3,65 +3,63 @@ class StackUsingLinkedList {
     static class Node{
         int data;
         Node next;
-
         Node(int data){
             this.data = data;
             this.next = null;
         }
     }
-
-    static class Stack{
+    static class Stack {
         static Node head = null;
 
-        //For Empty of stack
+        //Empty
         public boolean isEmpty(){
             return head == null;
         }
-
-        //for Push in Stack
+        
+        //PUsh
         public void push(int data){
             Node newNode = new Node(data);
             if(isEmpty()){
                 head = newNode;
                 return;
             }
-            newNode.next = head;
-            head = newNode;
+            
+            newNode.next = head; // basically we write here head.next = newNode (but here we implement stack so we go opposite so our latest value stay on top or first node)
+            head = newNode; 
+            // return;
         }
 
-        //For pop in Stack
+        //pop
         public int pop(){
             if(isEmpty()){
+                System.out.println("Underflow");
                 return -1;
             }
-
             int top = head.data;
             head = head.next;
             return top;
         }
 
-        //for Peek in Stack
+        //peek
         public int peek(){
-            if (isEmpty()){
+            if(isEmpty()){
+                System.out.println("Underflow");
                 return -1;
-            } 
+            }
             return head.data;
+            
         }
     }
-
-    public static void main(String[] args){
-        Stack temp = new Stack();
-        temp.push(10);
-        temp.push(20);
-        temp.push(30);
-        temp.push(40);
-        
-        while (!temp.isEmpty()){
-            System.out.println(temp.peek());
-            temp.pop();
-        } 
-
-        System.out.println("Stack is Empty: "+temp.peek());
-        
+    public static void main(String[] args) {
+        Stack stk = new Stack();
+        stk.push(23);
+        stk.push(13);
+        stk.push(43);
+        stk.push(123);
+        while (!stk.isEmpty()) {
+            System.out.println("Top Value: "+stk.peek());
+            stk.pop();
+        }
+        System.out.println("Stack is Empty: "+stk.isEmpty());
     }
 }
