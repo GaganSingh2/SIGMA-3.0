4c82f2da3de687356d5d8897d71a1a3131d400aa
GaganSingh2 <gks022004@gmail.com>
2025-12-29 00:16:51 +0530

    "Max Area of Histogram

--- Stack/MaxAreaOfHistogram.java
+++ Stack/MaxAreaOfHistogram.java
@@ -2,63 +2,61 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class MaxAreaOfHistogram {
-    private static void maxAreaOfHistogram(int height[]){
-        Stack<Integer> temp = new Stack<>();
-        int len = height.length;
-        int nSL[] = new int[len]; //Next Smaller Left
-        int nSR[] = new int[len]; //Next Smaller Right
 
-        //For Next Smaller Right
-        for(int i=len-1; i>=0; i--){
-            while(!temp.isEmpty() && height[temp.peek()]>=height[i]){
-                temp.pop();
+    public static int maxAreaOfHistogram(int area[]){
+        int len = area.length; 
+        int nsl[] = new int[len];
+        int nsr[] = new int[len];
+        Stack<Integer> tmp = new Stack<>();
+
+       
+        for(int i=0; i<nsl.length; i++){
+            while (!tmp.isEmpty() && area[i]<=area[tmp.peek()]) {
+                tmp.pop();
             }
-            if(temp.isEmpty()){
-                nSR[i] = height.length;
+            if(tmp.isEmpty()){
+                nsl[i] = -1;
             }
             else{
-                nSR[i] = temp.peek();
+                nsl[i] = tmp.peek();
             }
-            temp.push(i);
+            tmp.push(i);
         }
-
-        //For Next Smaller left
-        for(int i=0; i<height.length; i++){
-            while (!temp.isEmpty() && height[temp.peek()]>=height[i]) {
-                temp.pop();
+        for(int i=nsr.length-1; i>=0; i--){
+            while (!tmp.isEmpty() && area[i]<=area[tmp.peek()]) {
+                tmp.pop();
             }
-            if(temp.isEmpty()){
-                nSL[i] = -1;
+            if(tmp.isEmpty()){
+                nsr[i] = len;
             }
             else{
-                nSL[i] = temp.peek();
+                nsr[i] = tmp.peek();
             }
-            temp.push(i);
+            tmp.push(i);
         }
-
-        //For Finding Maximum Area
-        int maxArea = Integer.MIN_VALUE;
-        for(int i=0; i<height.length; i++){
-            int hei = height[i];
-            int Width = nSR[i]-nSL[i]-1;
-            int area = hei * Width;
-            maxArea = Math.max(maxArea, area);
+        int maxArea = 0;
+        for(int i=0; i<area.length; i++){
+            int width = nsr[i]-nsl[i]-1;
+            int height = area[i];
+            int currArea = height * width;
+            maxArea = Math.max(maxArea, currArea);
         }
-        System.out.println(maxArea);
+        return maxArea;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the Size: ");
-        int size = sc.nextInt();
-        int height[] = new int[size];
-        System.out.println("Enter the Heights: ");
-        for(int i=0; i<height.length; i++){
-            height[i] = sc.nextInt();
+        System.out.println("Enter the size: ");
+        int len = sc.nextInt();
+        int area[] = new int[len];
+        System.out.println("Enter the Area: ");
+        for(int i=0; i<len; i++){
+            area[i] = sc.nextInt();
         }
-        System.out.println("Height: ");
-        for(int i=0; i<height.length; i++){
-            System.out.print(height[i]+" ");
+        System.out.println("Area: ");
+        for(int i=0; i<len; i++){
+           System.out.print(area[i]+" ");
         }System.out.println();
-        maxAreaOfHistogram(height);
+        int maxArea = maxAreaOfHistogram(area);
+        System.out.println("Max Area of Histogram: "+maxArea);
     }
 }
