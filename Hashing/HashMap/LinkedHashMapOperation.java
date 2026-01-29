7c472fbb3897f34186c152cd4e9aedaee8bdf796
GaganSingh2 <gks022004@gmail.com>
2026-01-30 00:29:45 +0530

    "LinkedHashMap Operation (get(), remove(), keySet(), values())"

--- Hashing/HashMap/LinkedHashMapOperation.java
+++ Hashing/HashMap/LinkedHashMapOperation.java
@@ -6,11 +6,38 @@ public class LinkedHashMapOperation {
     public static void main(String[] args) {
         Map<Integer,String> lhm = new LinkedHashMap<>();
 
-        //Put Operation
+        //Put Operation O(1)
+        System.out.println("Put Operation--------");
         lhm.put(1, "Core Java");
         lhm.put(2, "Spring");
         lhm.put(3, "Hibernate");
         lhm.put(4, "Servlet");
         System.out.println(lhm);
+
+        //Get Operation O(1)
+        System.out.println("Get Operation----------");
+        System.out.println("Value of 3: "+lhm.get(3));
+        System.out.println("Value of 1: "+lhm.get(1));
+        System.out.println("Value of 5: "+lhm.get(5));
+
+        //Remove Operation (Delete Key-value)
+        System.out.println("Remove Operation---------");
+        lhm.remove(4);
+        lhm.remove(5);
+        System.out.println(lhm);
+
+
+        //KeySet Operation
+        System.out.println("keySet() Operation--------");
+        for(Integer k: lhm.keySet()){
+            System.out.println(k);
+        }
+
+        //values() Operation
+        System.out.println("values() Operation---------");
+        for(String val: lhm.values()){
+            System.out.println(val);
+        }
+
     }
 }
