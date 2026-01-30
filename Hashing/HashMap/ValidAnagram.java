71bb2ebf907123044cde03d2d68ac2f41b802844
GaganSingh2 <gks022004@gmail.com>
2026-01-31 01:50:52 +0530

    "Valid Anagram"

--- Hashing/HashMap/ValidAnagram.java
+++ Hashing/HashMap/ValidAnagram.java
@@ -4,8 +4,31 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class ValidAnagram {
+    //THis method throw ArrayIndexOutOfBoundException because we make the array only for small character because we use [ch-'a']
+    //Its work but firstly convert both string in lowercase but it is not recommended otherwise if constraint only in LowerCase or UpperCase then its work fine
+    public static boolean isValidAnagramUsingArray(String s, String t){
+        if(s.length() != t.length()){
+            return false;
+        }
+        int freq[] = new int[26];
+        for(int i=0; i<s.length(); i++){
+            char ch1 = s.charAt(i);
+            freq[ch1-'a']++;
+            char ch2 = t.charAt(i);
+            freq[ch2-'a']--;
+        }
+        
 
-    public static boolean isValidAnagram(String s, String t)
+        for(int ele: freq){
+            if(ele != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //THis is work for all
+    public static boolean isValidAnagramUsingHashMap(String s, String t)
     {
         if(s.length() != t.length()){
             return false;
@@ -45,7 +68,7 @@ public class ValidAnagram {
         System.out.println("Enter the second Str: ");
         String t = sc.nextLine();
 
-        boolean res = isValidAnagram(s, t);
+        boolean res = isValidAnagramUsingArray(s, t);
         if (res) {
             System.out.println("Valid Anagram!!");
         }
