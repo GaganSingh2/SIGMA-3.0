10fea24f0d9393d32364b0e8a32a9f8f426ac321
GaganSingh2 <gks022004@gmail.com>
2026-02-20 00:46:17 +0530

    "HashSet inbuilt Opearation"

---
+++ Hashing/HashSet/HashSetOperation.java
@@ -0,0 +1,51 @@
+package Hashing.HashSet;
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class HashSetOperation {
+    public static void main(String[] args) {
+        HashSet<Integer> set = new HashSet<>();
+
+        //Add The Key
+        System.out.println("Add Operation------");
+        set.add(10);
+        set.add(20);
+        set.add(30);
+        set.add(40);
+        set.add(50);
+        set.add(null);
+        System.out.println(set);
+
+        //Remove The Kye
+        System.out.println("Remove Operation--------");
+        set.remove(20);
+        System.out.println(set);
+
+        //Contains Key
+        System.out.println("Contains Key-----------");
+        System.out.println("30 is present: "+set.contains(30));
+        System.out.println("20 is Present: "+set.contains(20));
+
+        //Check The SIze of Key
+        System.out.println("Check SIze---------");
+        System.out.println("SIze: "+set.size());
+
+        //Display ALl the Key
+        System.out.println("Display All Key--------");
+        System.out.println("Using Iterator Interface:");
+        Iterator ite = set.iterator();
+        while (ite.hasNext()) {
+            System.out.println("Key: "+ite.next());
+        }
+
+        System.out.println("Using Advanced Loop: ");
+        for(Integer key: set){
+            System.out.println("Key: "+key);
+        }
+
+        //Clear The HashSet
+        System.out.println("Clear The HashSet--------");
+        set.clear();
+        System.out.println("Size: "+set.size());
+    }
+}
