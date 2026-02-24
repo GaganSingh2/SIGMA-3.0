37dc895e6a95f7b473f088ba2e7a99f5a04a7dbf
GaganSingh2 <gks022004@gmail.com>
2026-02-24 23:23:51 +0530

    "points"

--- NumberSystem/UniqueElement.java
+++ NumberSystem/UniqueElement.java
@@ -2,13 +2,6 @@ package NumberSystem;
 
 import java.util.*;
 public class UniqueElement {
-    public static int uniqueElement(int arr[]){
-        int unique = 0;
-        for(int ele: arr){
-            unique ^= ele;
-        }
-        return unique;
-    }
     /*
      * This Program Is Only Good if our Whole Array contain Positive Number 
      * Ex: IP-> 1 2 3 2 1, OP-> 3
@@ -16,9 +9,20 @@ public class UniqueElement {
      * This Program Is Only Good if our Whole Array contain Negative Number 
      * Ex: IP-> -1 -2 -3 -2 -1, OP-> -3
      * 
-     * This Program Is not Work Properly if our Whole Array contains contains the combination of Positive and Negatives values.
+     * This Program Is not Work Properly if our Array contains contains the combination of Positive and Negatives values.
      * Ex: IP-> -1 -2 3 -1 -2, OP-> 1(Wrong Ans)
      */
+    public static int uniqueElement(int arr[]){
+        int unique = 0;
+        for(int ele: arr){
+            unique ^= ele;
+        }
+        return unique;
+
+    }
+    
+    
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the length: ");
