51f73d39740b7a4180c51de5192d1297377be5f3
GaganSingh2 <gks022004@gmail.com>
2026-05-04 03:07:04 +0530

    "Practice ProblemStatement2"

--- TCSPractice/ProblemStatement2.java
+++ TCSPractice/ProblemStatement2.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class ProblemStatement2 {
     public static void main(String[] args) {
+        // Q)Given an integer aary arr of size n the task is to find the count of elements whose value is greater than all of its previous elemenets. Note: 1st element of array should be considered in the count of the result. for ex,arr[] = {7,4,8,2,9} as 7 is the first element, it will consider in result, 8 and 9 are also elements that are greater than all of its previous element, so answer is 3.
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
 
