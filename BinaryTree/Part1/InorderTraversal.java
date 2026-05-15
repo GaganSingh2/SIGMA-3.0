b55f8090697a8c8c87fb8ab5bd6d1fd8d22554c3
GaganSingh2 <gks022004@gmail.com>
2026-05-16 02:59:22 +0530

    "Inorder Traversal "

---
+++ BinaryTree/Part1/InorderTraversal.java
@@ -0,0 +1,53 @@
+package BinaryTree.Part1;
+
+public class InorderTraversal {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree{
+        static int idx = -1;
+        public static Node buildTree(int nodes[]){
+            idx++;
+            if (nodes[idx]==-1) {
+                return null;
+            }
+
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+
+        //Inorder Traversal(LeftSubTree -> RootNode -> RightSubTree)
+        public static void inorderTraversal(Node rootNode){
+            if (rootNode == null) {
+                System.out.print("-1 ");
+                return;
+            }
+
+            
+            inorderTraversal(rootNode.left);
+            System.out.print(rootNode.data+" ");
+            inorderTraversal(rootNode.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        BinaryTree tree = new BinaryTree();
+        Node rootNode = tree.buildTree(nodes);
+        System.out.println("RootNode: "+rootNode.data);
+
+        tree.inorderTraversal(rootNode);
+    }
+}
