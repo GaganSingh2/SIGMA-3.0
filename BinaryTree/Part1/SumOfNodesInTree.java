f778db5609b04b1e0152cb97aabf85956d4dd5a9
GaganSingh2 <gks022004@gmail.com>
2026-05-19 02:12:31 +0530

    "sumOfNodesUsingRecursion"

---
+++ BinaryTree/Part1/SumOfNodesInTree.java
@@ -0,0 +1,67 @@
+package BinaryTree.Part1;
+
+import java.util.Scanner;
+
+public class SumOfNodesInTree {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree{
+        static int idx = -1;
+        public static Node buildTree(int nodes[]){
+            idx++;
+            if (nodes[idx] == -1) {
+                return null;
+            }
+
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+
+        public static int sumOfNodesUsingRecursion(Node rootNode){
+
+            if (rootNode == null) {
+                return 0;
+            }
+
+             //find total sum of nodes of left subtree
+            int leftSum = sumOfNodesUsingRecursion(rootNode.left);
+
+            //find total sum of nodes of right subtree
+            int rightSum = sumOfNodesUsingRecursion(rootNode.right);
+
+            //then, leftSUm + rightSUm + rootNode.data
+            return leftSum + rightSum + rootNode.data;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // System.out.println("Number of Nodes: ");
+        // int len = sc.nextInt();
+
+        // int nodes[] = new int[len];
+        // System.out.println("Enter Data: ");
+        // for(int i=0; i<len; i++){
+        //     nodes[i] = sc.nextInt();
+        // }
+         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,4,-1,2,-1,-1};
+       
+        Node rootNode = BinaryTree.buildTree(nodes);
+        System.out.println("Root Node: "+rootNode.data);
+        
+        int sum = BinaryTree.sumOfNodesUsingRecursion(rootNode);
+        System.out.println("Total Sum of Nodes in Tree: "+sum);
+    }
+}
