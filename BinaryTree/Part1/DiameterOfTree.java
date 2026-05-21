e29d1e596e292dbca308907ff34b4f3a8374f2c5
GaganSingh2 <gks022004@gmail.com>
2026-05-21 18:49:05 +0530

    "Diameter of Tree in the form of Edges"

--- BinaryTree/Part1/DiameterOfTree.java
+++ BinaryTree/Part1/DiameterOfTree.java
@@ -1,8 +1,5 @@
 package BinaryTree.Part1;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 import BinaryTree.Part1.DiameterOfTree.BinaryTree.DiameterInfo;
 
 public class DiameterOfTree {
@@ -34,6 +31,7 @@ public class DiameterOfTree {
             return newNode;
         }
 
+         // Approach 1 to find the nodes TC: O(n^2), because here both height and diameter calculate one by one
         //Helper Method to find the Height of each SUbtree.
         public static int heightOfTree(Node rootNode){
             if (rootNode == null) {
@@ -47,7 +45,7 @@ public class DiameterOfTree {
             return Math.max(lefHeight, rightHeight)+1;
         }
 
-        // Approach 1 TC: O(n^2), because here both height and diameter calculate one by one
+       
         public static int diameterOfTree(Node rootNode){
             if (rootNode == null) {
                 return 0;
@@ -67,6 +65,7 @@ public class DiameterOfTree {
 
 
         //-------------------------------------------------------------------------------------------
+        //Approach-2 to find the nodes TC: O(n) bcz here we iterate only one time
         //Helper Class to store the Diameter and Height of Tree in the form of Object
         static class DiameterInfo{
             int diameter;
@@ -78,7 +77,7 @@ public class DiameterOfTree {
             }
         }
 
-        //Approach-2 TC: O(n) bcz here we iterate only one time
+        
         public static DiameterInfo diameterOfTree2(Node rootNode){
             if (rootNode == null) {
                 return new DiameterInfo(0, 0);
@@ -97,16 +96,53 @@ public class DiameterOfTree {
             return new DiameterInfo(finalDiameter, finalHeight);
         }
 
+
+        //Approach-1 (to find the edges) TC: O(n) bcz we iterate all nodes only one time 
+        static int diameter = 0;
+        public static int heightOfTreeInEdges(Node root){
+            if (root == null) {
+                return 0;
+            }
+
+            int lefHeight = heightOfTreeInEdges(root.left);
+            int rightHeight = heightOfTreeInEdges(root.right);
+
+            int currDiameter = lefHeight + rightHeight;
+
+            diameter = Math.max(diameter, currDiameter);
+
+            return Math.max(lefHeight, rightHeight) + 1;
+        }
+
+        public static int diameterOfTreeInEdges(Node rootNode){
+            if (rootNode == null) {
+                return 0;
+            }
+
+            heightOfTreeInEdges(rootNode);
+            return diameter;
+        }
+
     }
     public static void main(String[] args) {
         // int nodes[] = {1,2,4,9,-1,-1,-1,5,-1,6,-1,7,-1,-1,3,-1,-1};
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,6,-1,-1,7,-1,-1};
 
+       
         Node rootNode = BinaryTree.buildBinaryTree(nodes);
         System.out.println("Root Node: "+rootNode.data);
 
+         //Approach-1 (to find the nodes)
+         int maxDiameterInNode = BinaryTree.diameterOfTree(rootNode);
+         System.out.println("Maximum Diameter(in nodes): "+maxDiameterInNode);
+
+        //Approach-2 (to find the nodes)
         DiameterInfo info = BinaryTree.diameterOfTree2(rootNode);
-        System.out.println("Maximum Diameter: "+info.diameter);
+        System.out.println("Maximum Diameter(in nodes): "+info.diameter);
+
+        //Approach-1 (to find the edges)
+        int maxDiameterInEdges = BinaryTree.diameterOfTreeInEdges(rootNode);
+        System.out.println("Maximum Diamter(in Edges): "+maxDiameterInEdges);
     }
 }
 
@@ -115,7 +151,7 @@ Diameter of Tree means:
 * It is the length of longest path between any two nodes in tree.
 * The path: 
     * May or may not pass through the root.
-    * Is measured in edges.
+    * Is measured in edges or measured in nodes.
     *                   1
     *                  / \
     *                 2   3
@@ -125,7 +161,8 @@ Diameter of Tree means:
     *             9       6
     *                      \
     *                       7
-    * Diameter = 6 
+    * Diameter = 6 (nodes)
+    * Diameter = 5 (edges)
     * (7->6->5->2->1->3) pass through the Root Node
     * (9->4->2->5->6->7) not pass through the Root Node
     * 
