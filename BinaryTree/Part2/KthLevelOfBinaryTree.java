28426c021471b2240881fdd00387ce097e884829
GaganSingh2 <gks022004@gmail.com>
2026-05-25 20:22:08 +0530

    "kth Level Of Binary Tree Using Level Order Traversal"

--- BinaryTree/Part2/KthLevelOfBinaryTree.java
+++ BinaryTree/Part2/KthLevelOfBinaryTree.java
@@ -1,5 +1,8 @@
 package BinaryTree.Part2;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class KthLevelOfBinaryTree {
     static class Node{
         int data; 
@@ -28,8 +31,9 @@ public class KthLevelOfBinaryTree {
             return newNode;
         }
 
+        //1st Approach -----------------------
         //Print Kth Level of Binary Tree using Recursion TC:O(n).
-        public static void kthLevelOfBinaryTree(Node rootNode, int level, int k){
+        public static void kthLevelOfBinaryTreeUsingRecursion(Node rootNode, int level, int k){
             if(rootNode == null || k<1){
                 return;
             }
@@ -38,18 +42,63 @@ public class KthLevelOfBinaryTree {
                 return; //so after this we don't want to go to again child node of kth node
             }
 
-            kthLevelOfBinaryTree(rootNode.left, level+1, k);
-            kthLevelOfBinaryTree(rootNode.right, level+1, k);
+            kthLevelOfBinaryTreeUsingRecursion(rootNode.left, level+1, k);
+            kthLevelOfBinaryTreeUsingRecursion(rootNode.right, level+1, k);
 
+        }
+
+
+        //2nd Approach ----------------------------------
+        //Print kth Level of Binary Tree using Level Order Traversal TC: O(n).
+        public static void kthLevelOfBinaryTreeUsingLevelOrderTraversal(Node rootNode, int k){
+            if (rootNode == null || k<1) {
+                System.out.println("Root Node null/ k is less than 1");
+                return;
+            }
+
+            Queue<Node> q = new LinkedList<>();
+            q.add(rootNode);
+            q.add(null);
+            int tempLevel = 1;
+
+            while (!q.isEmpty()) {
+                Node currNode = q.remove();
+                if (currNode == null) {
+                    if (q.isEmpty()) {
+                        return;
+                    }
+                    else{
+                        if (k == tempLevel) {
+                            return; //Already kth Level print so we don't have need to check again, return out from the method
+                        }
+                        q.add(null);
+                        tempLevel++;
+                    }
+                }
+                else{
+                    if (k == tempLevel) {
+                        System.out.print(currNode.data+" ");
+                        continue; //after reaching to kth level we don't want to add again next level data
+                    }
+                    if (currNode.left != null) {
+                        q.add(currNode.left);
+                    }
+                    if (currNode.right != null) {
+                        q.add(currNode.right);
+                    }
+                }
+            }
         }
     }
     public static void main(String[] args) {
         int nodes[] = {20,8,5,-1,-1,3,10,-1,-1,14,-1,-1,22,-1,25,-1,-1};
-        int k = 3;
+        int k = -7;
         Node rootNode = BinaryTree.buildBinaryTree(nodes);
         System.out.println("Root Node: "+rootNode.data);
 
-        BinaryTree.kthLevelOfBinaryTree(rootNode, 1, k);
+        // BinaryTree.kthLevelOfBinaryTreeUsingRecursion(rootNode, 1, k);
+
+        BinaryTree.kthLevelOfBinaryTreeUsingLevelOrderTraversal(rootNode, k);
     }
 }
 
