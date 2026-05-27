f0dfb054981e2c1ca66a7d493080fa73f6690caf
GaganSingh2 <gks022004@gmail.com>
2026-05-27 19:03:06 +0530

    "Kth Ancestor Of Node In Binary Tree by using Path Based Approach"

--- BinaryTree/Part2/KthAncestorOfNodeInBinaryTree.java
+++ BinaryTree/Part2/KthAncestorOfNodeInBinaryTree.java
@@ -1,5 +1,7 @@
 package BinaryTree.Part2;
 
+import java.util.ArrayList;
+
 public class KthAncestorOfNodeInBinaryTree {
     static class Node{
         int data;
@@ -26,8 +28,9 @@ public class KthAncestorOfNodeInBinaryTree {
             return newNode;
         }
 
+        //1st Approach------------------------------------------
         //Find the Kth Ancestor of given node by using Recursion
-        public static int kthAncestorOfNode(Node rootNode, int node, int k){
+        public static int kthAncestorOfNodeByUsingRecursion(Node rootNode, int node, int k){
             if (rootNode == null || k<1) {
                 return -1;
             }
@@ -36,10 +39,10 @@ public class KthAncestorOfNodeInBinaryTree {
             }
 
             //find the given node distance in leftSubTree, if node not found then gives -1
-            int leftDist = kthAncestorOfNode(rootNode.left, node, k);
+            int leftDist = kthAncestorOfNodeByUsingRecursion(rootNode.left, node, k);
 
             //find the given node distance in rightSubTree, if node not found then gives -1
-            int rightDist = kthAncestorOfNode(rootNode.right, node, k);
+            int rightDist = kthAncestorOfNodeByUsingRecursion(rootNode.right, node, k);
             
             if (leftDist == -1 && rightDist == -1) {//if both -1 then return -1
                 return -1;
@@ -53,17 +56,64 @@ public class KthAncestorOfNodeInBinaryTree {
             }
             return maxDist + 1;
         }
+
+        //2nd Approach By using Path Based Approach----------------
+        //Helper method to store nodes form rootNode to given node
+        public static boolean getPath(Node rootNode, int node, ArrayList<Node> path){
+            if (rootNode == null) {
+                return false;
+            }
+
+            path.add(rootNode);
+            if (rootNode.data == node) {
+                return true;
+            }
+
+            boolean foundLeft = getPath(rootNode.left, node, path);
+            boolean foundRight = getPath(rootNode.right, node, path);
+
+            if (foundLeft || foundRight) {
+                return true;
+            }
+            path.remove(path.size()-1);
+            return false;
+        }
+        
+        //Find kth Ancestor by using Path based
+        public static int kthAncestorOfNodeByUsingPathBased(Node rootNode, int node, int k){
+            if (k<0) { //if kth position is less than 0 means invalid
+                return -1;
+            }
+            ArrayList<Node> path = new ArrayList<>();
+
+            boolean foundPath = getPath(rootNode, node, path);//store the path from rootNode to given node if its return true means path exist, otherwise not
+
+            if (!foundPath) { //path not exist in Tree
+                return -1;
+            }
+            int index = path.size()-k-1;
+            if (index < 0) {
+                return -1; //kth Ancestor not exist
+            }
+
+            return path.get(index).data;
+        }
     }
     public static void main(String[] args) {
-        int node = 16, k = 0;
+        int node = 5, k = 5;
         int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, 6, -1, -1, 7, -1, -1 };
         Node rootNode = BinaryTree.buildBinaryTree(nodes);
         System.out.println("Root Node: "+rootNode.data);
 
-        int res = BinaryTree.kthAncestorOfNode(rootNode, node, k);
-        if (res == -1) {
-            System.out.println("Node not Found");
-        }
+        //1st Approach by Using Recursion-----------
+        // int res = BinaryTree.kthAncestorOfNodeByUsingRecursion(rootNode, node, k);
+        // if (res == -1) {
+        //     System.out.println("Node not Found");
+        // }
+
+        //2nd Approach by using Path Based--------------
+        int res = BinaryTree.kthAncestorOfNodeByUsingPathBased(rootNode, node, k);
+        System.out.println(k+"th Ancestor of node "+node+": "+res);
     }
 }
 
