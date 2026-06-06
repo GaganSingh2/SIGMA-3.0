479a2480d8c9455a7ab9093a594e5919ca6cae56
GaganSingh2 <gks022004@gmail.com>
2026-06-06 10:56:25 +0530

    "maximum Product of 2 value Without using Sorting"

--- Test/MaxProductOf2Value.java
+++ Test/MaxProductOf2Value.java
@@ -11,6 +11,37 @@ public class MaxProductOf2Value {
         int max2 = arr[arr.length-1] *  arr[arr.length-2];
         return Math.max(max1, max2);
     }
+
+
+    public static int maxProductWithoutSorting(int arr[]){
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+
+        for(int num: arr){
+            //two largest number of array
+            if (num>max1) {
+                max2 = max1;
+                max1 = num;
+            }
+            else if(num>max2){
+                max2 = num;
+            }
+
+            //two smallest number of array
+            if (num<min1) {
+                min2 = min1;
+                min1 = num;
+            }
+            else if(num<min2){
+                min2 = num;
+            }
+        }
+
+        return Math.max(max1*max2, min1*min2);
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter length: ");
@@ -27,5 +58,7 @@ public class MaxProductOf2Value {
         }System.out.println();
 
         System.out.println("Maximum Product Using Sorting: "+maxProductUsingSorting(arr));
+
+        System.out.println("Maximum Product Without Using Sorting: "+maxProductWithoutSorting(arr));
     }
 }
